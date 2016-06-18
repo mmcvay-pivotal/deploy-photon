9948fb63c3019dbd00a5cb70e163705023076758
boparp-pivotal <pbopardikar@pivotal.io>
2016-06-17 22:27:18 -0700

    hardcoded cli_url removed

--- ci/tasks/download-cli.sh
+++ ci/tasks/download-cli.sh
@@ -3,7 +3,6 @@ set -e
 
 # Get Photon CLI
 echo "Downloading Photon CLI..."
-cli_url="https://bintray.com/photon-controller/esxcloud-archives/download_file?file_path=v0.9.0%2F106%2Fcli%2Fbin%2Flinuxamd64%2Fphoton"
 if [[ $cli_url == "latest" || -z "$cli_url" ]]; then
   echo "Using default url."
   cli_url=$(wget -q -O- https://github.com/vmware/photon-controller/wiki/download | grep "linux cli tools" | egrep -o http.*\" | tr -d "\"")
